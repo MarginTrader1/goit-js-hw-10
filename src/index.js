de1a593862e2426e368ede58f057836fa28132ed
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-06-13 02:35:07 +0300

    Update index.js
    Добавил породу в разметку

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ new SlimSelect({
 })
 
 const breedList = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 const KEY_API = 'live_sEDFMHRNSodHCaL7awGvLr0m2GM2ft2fhMh0p4uHuGLReqOu194EfLuWmNYD9tGw';
 const url = `https://api.thecatapi.com/v1/breeds`;
+
 
 // вызов функции при загрузке страницы
 document.addEventListener("DOMContentLoaded", () => { 
@@ -40,6 +42,60 @@ function renderBreedsList(breeds) {
   breedList.innerHTML = markup;
 }
 
-function getCat(){
+// получаем кота по введенной породе 
+function fetchCatByBreed(breedId){
   
+  const urlСatByBreed = `https://api.thecatapi.com/v1/images/search?api_key=${KEY_API}&limit=1&breed_ids=${breedId}`
+  
+  fetch(urlСatByBreed)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(json => renderCatInfo(json)) 
+    .catch(error => {
+      // Error handling
+    });
+  }
+
+// создания разметки для информации о породе 
+function renderCatInfo(breedData) {
+  
+  // добавляем url
+  const markupUrl = breedData
+    .map((item) => {
+      return `
+      <img src="${item.url}" width="500">              
+      `;
+  });
+
+  // вытягиваем и добавляем описание и имя
+  const markupName = breedData
+    .flatMap(elem => elem.breeds)
+    .map((item) => {
+      return `
+      <h2>${item.name}</h2>
+      <p>${item.description}/p>              
+      `;
+  });
+
+  // вставляем разметку в код
+  catInfo.innerHTML = markupUrl + markupName;
 }
+
+// получаем выбранное значение породы 
+function getSelectBreed(e){
+  let breedId = e.target.value;
+  fetchCatByBreed(breedId);
+}
+
+breedList.addEventListener('change', getSelectBreed)
+
+
+
+
+
+
+  
