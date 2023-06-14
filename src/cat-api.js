1ee7975211fb7c11dcf09feb319ca347ad4f062f
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-06-14 10:55:29 +0300

    переделал код и добавил разметку

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,3 @@
-const catInfo = document.querySelector('.cat-info');
-const KEY_API = 'live_sEDFMHRNSodHCaL7awGvLr0m2GM2ft2fhMh0p4uHuGLReqOu194EfLuWmNYD9tGw';
-
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
 
 // получаем породы и вызываем функцию создания разметки 
 function fetchBreeds(url) {
@@ -11,75 +6,23 @@ function fetchBreeds(url) {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    error.classList.add("visually-hidden")
-    loader.classList.add("visually-hidden")
     return response.json();
   })
-  .then(json => renderBreedsList(json))
-  .catch(error => {
-    // Error handling
-    error.classList.remove("visually-hidden")
-  });
 }   
-
-// функция создания разметки при перезагрузке страницы 
-function renderBreedsList(breeds) {
-  
-  const breedList = document.querySelector('.breed-select');
-  
-  const markup = breeds
-    .map((breed) => {
-      return `
-      <option value="${breed.id}">${breed.name}</option>
-      `;
-    })
-    .join("");
-  breedList.innerHTML = markup;
-}
 
 // получаем кота по введенной породе 
 function fetchCatByBreed(breedId){
   
+  const KEY_API = 'live_sEDFMHRNSodHCaL7awGvLr0m2GM2ft2fhMh0p4uHuGLReqOu194EfLuWmNYD9tGw';
   const urlСatByBreed = `https://api.thecatapi.com/v1/images/search?api_key=${KEY_API}&limit=1&breed_ids=${breedId}`
-  
-  fetch(urlСatByBreed)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      error.classList.add("visually-hidden")
-      return response.json();
-    })
-    .then(json => renderCatInfo(json)) 
-    .catch(error => {
-      // Error handling
-    });
+
+  return fetch(urlСatByBreed)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
   }
-
-// создания разметки для информации о породе 
-function renderCatInfo(breedData) {
-  
-  // добавляем url
-  const markupUrl = breedData
-    .map((item) => {
-      return `
-      <img src="${item.url}" width="500">              
-      `;
-  });
-
-  // вытягиваем и добавляем описание и имя
-  const markupName = breedData
-    .flatMap(elem => elem.breeds)
-    .map((item) => {
-      return `
-      <h2>${item.name}</h2>
-      <p>${item.description}</p>              
-      `;
-  });
-
-  // вставляем разметку в код
-  catInfo.innerHTML = markupUrl + markupName;
-}
-
 
 export { fetchBreeds,fetchCatByBreed }; 
--- src/index.js
+++ src/index.js
@@ -6,18 +6,71 @@ new SlimSelect({
 })
 
 const breedList = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
 const url = `https://api.thecatapi.com/v1/breeds`;
 
+
 // вызов функции при загрузке страницы
 document.addEventListener("DOMContentLoaded", () => { 
-  fetchBreeds(url);
+  fetchBreeds(url)
+  .then(json => renderBreedsList(json))
+  .catch(error => {
+    // Error handling
+  });;
 });
+
+// функция создания разметки при перезагрузке страницы 
+function renderBreedsList(breeds) {
+  const markup = breeds
+    .map((breed) => {
+      return `
+      <option value="${breed.id}">${breed.name}</option>
+      `;
+    })
+    .join("");
+  breedList.innerHTML = markup;
+}
 
 // получаем выбранное значение породы 
 function getSelectBreed(e){
   const breedId = e.target.value;
-  fetchCatByBreed(breedId);
+  
+  fetchCatByBreed(breedId)
+  .then(json => renderCatInfo(json)) 
+  .catch(error => {
+    // Error handling
+  });;
+}
+
+// создания разметки для информации о породе 
+function renderCatInfo(breedData) {
+  
+  // добавляем url
+  const markupUrl = breedData
+    .map((item) => {
+      return `
+      <img src="${item.url}" width="500">              
+      `;
+  });
+
+  // вытягиваем и добавляем описание и имя
+  const markupName = breedData
+    .flatMap(elem => elem.breeds)
+    .map((item) => {
+      return `
+      <div>
+        <h2>${item.name}</h2>
+        <p>${item.description}</p>  
+        <p><span style="font-size:120%; font-weight:bold;">Temperament:</span> ${item.temperament}.</p>
+      </div>              
+      `;
+  });
+
+  // вставляем разметку в код
+  catInfo.innerHTML = markupUrl + markupName;
 }
 
 breedList.addEventListener('change', getSelectBreed)
