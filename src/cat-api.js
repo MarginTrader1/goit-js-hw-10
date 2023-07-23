9de30eec32b855e40ee8b320ed3cfa5fe3b6e906
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-07-23 16:02:21 +0300

    format

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,18 +1,15 @@
-
-// получаем породы 
+// получаем породы
 function fetchBreeds(url) {
-  return fetch(url)
-  .then(response => response.json())
-}   
+  return fetch(url).then(response => response.json());
+}
 
-// получаем кота по введенной породе 
-function fetchCatByBreed(breedId){
-  
-  const KEY_API = 'live_sEDFMHRNSodHCaL7awGvLr0m2GM2ft2fhMh0p4uHuGLReqOu194EfLuWmNYD9tGw';
-  const urlСatByBreed = `https://api.thecatapi.com/v1/images/search?api_key=${KEY_API}&limit=1&breed_ids=${breedId}`
+// получаем кота по введенной породе
+function fetchCatByBreed(breedId) {
+  const KEY_API =
+    'live_sEDFMHRNSodHCaL7awGvLr0m2GM2ft2fhMh0p4uHuGLReqOu194EfLuWmNYD9tGw';
+  const urlСatByBreed = `https://api.thecatapi.com/v1/images/search?api_key=${KEY_API}&limit=1&breed_ids=${breedId}`;
 
-  return fetch(urlСatByBreed)
-  .then(response => response.json())
-  }
+  return fetch(urlСatByBreed).then(response => response.json());
+}
 
-export { fetchBreeds, fetchCatByBreed }; 
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import Notiflix from 'notiflix';
 
 new SlimSelect({
-  select: '#selectElement'
-})
+  select: '#selectElement',
+});
 
 const breedList = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
@@ -15,25 +15,24 @@ const error = document.querySelector('.error');
 const url = `https://api.thecatapi.com/v1/breeds`;
 
 // вызов функции при загрузке страницы
-document.addEventListener("DOMContentLoaded", () => { 
-  
+document.addEventListener('DOMContentLoaded', () => {
   fetchBreeds(url)
-  .then(json => {
-    // делаем проверку получения данных
-    // если нет данных - тогда ошибка 
-    if('message' in json) {
-      loader.classList.add('visually-hidden')
-      throw new Error();
-      } 
+    .then(json => {
+      // делаем проверку получения данных
+      // если нет данных - тогда ошибка
+      if ('message' in json) {
+        loader.classList.add('visually-hidden');
+        throw new Error();
+      }
       //если данные есть - рендерим разметку
-      breedList.classList.remove('visually-hidden')
-      loader.classList.add('visually-hidden')
-      renderBreedsList(json)
-  })
-  .catch(error => onError(error));
+      breedList.classList.remove('visually-hidden');
+      loader.classList.add('visually-hidden');
+      renderBreedsList(json);
+    })
+    .catch(error => onError(error));
 });
 
-// функция создания разметки при перезагрузке страницы 
+// функция создания разметки при перезагрузке страницы
 function renderBreedsList(breeds) {
   const markup = breeds
     .map(({ id, name }) => {
@@ -41,40 +40,37 @@ function renderBreedsList(breeds) {
       <option value="${id}">${name}</option>
       `;
     })
-    .join("");
+    .join('');
   breedList.innerHTML = markup;
-  
 }
 
-// получаем выбранное значение породы 
-function getSelectBreed(e){
+// получаем выбранное значение породы
+function getSelectBreed(e) {
   const breedId = e.target.value;
-  
-  loader.classList.remove('visually-hidden')
-  catInfo.classList.add('visually-hidden')
+
+  loader.classList.remove('visually-hidden');
+  catInfo.classList.add('visually-hidden');
 
   fetchCatByBreed(breedId)
-  .then(json => {
-    if (json.length === 0) {
-      // делаем проверку данных
-      loader.classList.add('visually-hidden')
-      throw new Error();
-    }
-    //если данные есть - рендерим разметку
-    loader.classList.add('visually-hidden')
-    renderCatInfo(json)
-    catInfo.classList.remove('visually-hidden')
-  }) 
-  .catch(error => onError(error));
+    .then(json => {
+      if (json.length === 0) {
+        // делаем проверку данных
+        loader.classList.add('visually-hidden');
+        throw new Error();
+      }
+      //если данные есть - рендерим разметку
+      loader.classList.add('visually-hidden');
+      renderCatInfo(json);
+      catInfo.classList.remove('visually-hidden');
+    })
+    .catch(error => onError(error));
 }
 
-// функция создания разметки для информации о породе 
+// функция создания разметки для информации о породе
 function renderCatInfo(breedData) {
-
   // добавляем url
-  const markupUrl = breedData
-    .map(({ url }) => {
-      return `
+  const markupUrl = breedData.map(({ url }) => {
+    return `
       <img src="${url}" width="500">              
       `;
   });
@@ -90,25 +86,18 @@ function renderCatInfo(breedData) {
         <p><span style="font-size:120%; font-weight:bold">Temperament:</span> ${temperament}.</p>
       </div>              
       `;
-  });
+    });
 
   // вставляем разметку в код
   catInfo.innerHTML = markupUrl + markupName;
 }
 
 // функция вывода ошибки
-function onError(err){
+function onError(err) {
   error.classList.remove('visually-hidden');
-  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
 }
 
-breedList.addEventListener('change', getSelectBreed)
-
-
-
-
-
-
-
-
-  
+breedList.addEventListener('change', getSelectBreed);
